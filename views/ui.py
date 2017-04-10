9d2d2f67de9318147721cf9cb18f34a974be5dc3
georgreen <geogreenmanu@gmail.com>
2017-04-11 01:25:45 +0300

    Chore: add views to project structure

---
+++ views/ui.py
@@ -0,0 +1,11 @@
+def print_welcome():
+	'''
+	display welcome message
+	'''
+	print_message("Welcome to Office Space Allocation")
+
+def print_message(msg):
+	'''
+	print message on consle
+	'''
+	print(msg)
