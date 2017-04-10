498c31f0212b3e6dbe3ff124c8c123ec9a2d110b
georgreen <geogreenmanu@gmail.com>
2017-04-10 20:27:47 +0300

    Refactored code to handle non-standard inputs

--- core/logic.py
+++ core/logic.py
@@ -10,9 +10,21 @@ def create_room(room_type, room_name):
     Raises -> TypeError if room_name exists
             'Invalid name ' if room_name exists
     """
-    datatype = {'office' : model.Office, 'livingspace' : model.LivingSpace}
-    if room_type.lower() in datatype:
-        if room_name:
-            return datatype[room_type.lower()](room_name)
+    #remove excess white charcters
+    room_name_stripped =  room_name.split()
+    room_type_stripped = room_type.split()
+
+    if len(room_type_stripped) == 0:
+        raise TypeError
+    room_type_cleaned = room_type_stripped[0]
+
+    if len(room_name_stripped) == 0:
         return 'Invalid name'
+    room_name_cleaned = room_name_stripped[0]
+
+    #map room_type to respective data type
+    datatype = {'office' : model.Office, 'livingspace' : model.LivingSpace}
+
+    if room_type_cleaned.lower() in datatype:
+        return datatype[room_type_cleaned.lower()](room_name_cleaned)
     raise TypeError
