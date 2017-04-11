6296dc3490b8abf03b46d75ce56ff1531ddc673b
georgreen <geogreenmanu@gmail.com>
2017-04-11 16:05:44 +0300

    Feat: Implemented a helper to link model:room and feat:create_room

--- core/logic.py
+++ core/logic.py
@@ -1,6 +1,15 @@
 from context import models
 from models import model
 
+dojo = None
+def init_app():
+    #create dojo
+    dojo = model.Dojo("Andela-Kenya")
+
+#create a dojo
+if not dojo:
+    dojo = init_app()
+
 
 def create_room(room_type, room_name):
     """
@@ -11,16 +20,16 @@ def create_room(room_type, room_name):
             'Invalid name ' if room_name exists
     """
     #remove excess white charcters
-    room_name_stripped =  room_name.split()
-    room_type_stripped = room_type.split()
+    room_name_stripped =  room_name.strip()
+    room_type_stripped = room_type.strip()
 
     if len(room_type_stripped) == 0:
         raise TypeError
-    room_type_cleaned = room_type_stripped[0]
+    room_type_cleaned = room_type_stripped
 
     if len(room_name_stripped) == 0:
         return 'Invalid name'
-    room_name_cleaned = room_name_stripped[0]
+    room_name_cleaned = room_name_stripped
 
     #map room_type to respective data type
     datatype = {'office' : model.Office, 'livingspace' : model.LivingSpace}
@@ -29,7 +38,20 @@ def create_room(room_type, room_name):
         return datatype[room_type_cleaned.lower()](room_name_cleaned)
     raise TypeError
 
-def add_person(*args):
+def helper_create_and_addroom(room_type, room_name):
+    new_room = create_room(room_type, room_name)
+    if isinstance(room, model.Office):
+        #add to Dojo Office
+        pass
+    elif isinstance(room, model.LivingSpace):
+        #add to Dojo LivingSpace
+        pass
+    elif new_room == 'Invalid name':
+        #give some status messge
+        pass
+
+
+def add_person():
     """
     input: firstname lastname Fellow/Staff [Y]
     """
