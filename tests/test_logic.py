dedfd7581b1d039c54fea574e512fbf5aa2cb06d
georgreen <geogreenmanu@gmail.com>
2017-04-11 22:21:31 +0300

    Test: Implemented test for add_person feature

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -37,4 +37,35 @@ class test_create_room(unittest.TestCase):
 
 
 class test_add_person(unittest.TestCase):
-    pass
+    """
+    Testing strategy
+    partitioons:
+        @input
+        name,person_type,
+             -> len(input) -> 0 , postive,
+             -> name : whitespace, str(number), asscii charcters
+             --> bad types
+               -> bad types, double
+        livingspace
+                -> True
+                -> False
+                -> bad type
+        Each test should be covered atleast once
+    """
+    def test_create_person(self):
+        self.assertIsInstance(logic.add_person("new_staff", 'staff'), model.Staff)
+        self.assertIsInstance(logic.add_person("new_fellow", 'fellow'))
+        self.assertIsInstance(logic.add_person("new_fellow_livingspace", "fellow", "Y"))
+
+    def test_wrong_type(self):
+        with self.assertRaises(TypeError):
+            logic.add_person("new_staff", [] )
+        with self.assertRaises(TypeError):
+            logic.add_person("new_fellow", {})
+        with self.assertRaises(TypeError):
+            logic.add_person("new_fellow", "fellow", 4)
+
+    def test_non_stanard_input(self):
+        self.assertEqual(logic.add_person(" ", "staff"), 'Invalid name')
+        self.assertEqual(logic.add_person("new_name    "), "fellow", "Y")
+        self.assertEqual(logic.add_person(";;;;;;@@@", 'fellow'), "Invalid name")
