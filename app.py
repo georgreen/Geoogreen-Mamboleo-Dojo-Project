11b8a38dfe47226e55197a51c5e98bdacff182c2
georgreen <geogreenmanu@gmail.com>
2017-04-14 23:28:24 +0300

    Feat: added load people feature

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ Options:
 '''
 
 
-from context import models, views, core, os
-from core import logic
-from views import ui
+import os
 from docopt import docopt, DocoptExit
 import cmd
 from models import model
 from pyfiglet import figlet_format
 from termcolor import cprint, colored
 import csv
+from views import ui
+from core import logic
 
 
 
@@ -224,16 +224,20 @@ class App(cmd.Cmd):
     def do_load_people(self, args):
         """
         Usage:
-            reallocate_person <person_identifier> <new_room_name>
+            load_people <file_name>
         """
         try:
-            reallocate_information = docopt(self.do_reallocate_person.__doc__, args)
+            file_name = docopt(self.do_reallocate_person.__doc__, args)
 
         except DocoptExit as e:
             ui.print_message(e)
             #call view to display Error message
         except KeyboardInterrupt:
             pass
+        else:
+            file_name = file_name['<file_name>']
+            status_messages = logic.load_data_txt(file_name, App.dojo)
+            print(status_messages)
 
 
 
