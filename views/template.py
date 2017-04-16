9b7b797c4abc9a16eeca75eedb2b7a86f2e203e8
georgreen <geogreenmanu@gmail.com>
2017-04-17 01:12:07 +0300

    Chore: Added templates for add person UI

--- views/template.py
+++ views/template.py
@@ -3,3 +3,12 @@ room_created_message = "A %s called %s has been successfully created!"
 room_not_created_message = "A %s called %s can not be created!:Invalid Name"
 room_dup_name_message = "A %s called %s can not be created!: Name already exists"
 room_typeerror_message = "Room type: %s, Name: %s can not be  created!"
+
+person_created_message = "{} {} {} has been successfully added."
+person_not_created_message = "{} {} {} has not added."
+
+allocated_office_message = "{} has been allocated the office {}"
+not_allocated_office_message = "{} not has been allocated the office"
+
+allocated_living_message = "{} has been allocated the livingspace {} "
+not_allocated_living_message = "{} not has been allocated a livingspace"
