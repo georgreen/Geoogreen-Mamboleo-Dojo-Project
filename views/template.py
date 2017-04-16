dadc894da882f6d52b48e05a1bd09e3f401756a4
georgreen <geogreenmanu@gmail.com>
2017-04-16 20:20:06 +0300

    Chore: Refactored UI status messages to templates

---
+++ views/template.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+room_created_message = "A %s called %s has been successfully created!"
+room_not_created_message = "A %s called %s can not be created!:Invalid Name"
+room_dup_name_message = "A %s called %s can not be created!: Name already exists"
+room_typeerror_message = "Room type: %s, Name: %s can not be  created!"
