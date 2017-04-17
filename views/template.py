61facc916bdb6bfb3af1c0c0ea2ce0d3e25fe0af
georgreen <geogreenmanu@gmail.com>
2017-04-17 16:40:43 +0300

    Chore: Added code from logic

--- views/template.py
+++ views/template.py
@@ -4,11 +4,11 @@ room_not_created_message = "A %s called %s can not be created!:Invalid Name"
 room_dup_name_message = "A %s called %s can not be created!: Name already exists"
 room_typeerror_message = "Room type: %s, Name: %s can not be  created!"
 
-person_created_message = "{} {} {} has been successfully added."
-person_not_created_message = "{} {} {} has not added."
+person_created_message = "%s %s  has been successfully added."
+person_not_created_message = "%s %s was not added. Person Type: %s is not available"
 
-allocated_office_message = "{} has been allocated the office {}"
-not_allocated_office_message = "{} not has been allocated the office"
+allocated_office_message = "%s has been allocated the office %s"
+not_allocated_office_message = "%s has not been allocated the office"
 
-allocated_living_message = "{} has been allocated the livingspace {} "
-not_allocated_living_message = "{} not has been allocated a livingspace"
+allocated_living_message = "%s has been allocated the livingspace %s"
+not_allocated_living_message = "%s has not been allocated a livingspace"
