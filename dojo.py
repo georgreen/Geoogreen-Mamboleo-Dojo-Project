d9a05dd6d28889753b3e675dd7d3080ce19670a4
georgreen <geogreenmanu@gmail.com>
2017-04-18 13:28:32 +0300

    Chore: Refactored code for more readability

--- dojo.py
+++ dojo.py
@@ -46,24 +46,8 @@ class Dojo(cmd.Cmd):
             room_type = room_information['<room_type>']
             for name in room_information['<room_name>']:
                 status_message = logic.create_and_addroom(self.dojo, room_type, name)
+                ui.room_ui(status_message)
 
-                # call ui from views to display our status messages
-                room_type = status_message['room_type']
-                room_name = status_message['room_name']
-                status = status_message['status']
-                messsage = None
-                color = None
-                fail = True
-                if status_message['status'] == 'ok':
-                    message = template.room_created_message
-                    fail = False
-                elif status_message['status'] == 'failed':
-                    message = template.room_typeerror_message
-                elif status_message['status'] == 'Duplicate name':
-                    message = template.room_dup_name_message
-                elif status_message['status'] == 'Invalid name':
-                    message = template.room_not_created_message
-                ui.print_template_room(message, room_type, room_name, fail=fail, status=status)
 
     def do_add_person(self, args):
         """
@@ -81,39 +65,7 @@ class Dojo(cmd.Cmd):
             person_type = person_information['<person_type>']
 
             status_messages = logic.addsperson_chooseroom(self.dojo,firstname, secondname, person_type, wants_room)
-            self.person_ui(status_messages)
-
-    def person_ui(self, status_messages):
-        status = status_messages['status']
-        name = status_messages['name']
-        person_type = status_messages['person_type']
-
-        if status == 'Failed':
-            message = template.person_not_created_message
-            ui.print_template_room(message, person_type, name, person_type, fail=True, status='Failed')
-        elif status == 'ok':
-            choice = status_messages['choice_live']
-            office = status_messages['office']
-            livingspace = status_messages['livingspace']
-
-            # dispaly person created flush
-            message = template.person_created_message
-            ui.print_template_room(message, person_type, name)
-
-            if office:
-                message = template.allocated_office_message
-                ui.print_template_room(message, name, office)
-            else:
-                message = template.not_allocated_office_message
-                ui.print_template_room(message, name, fail=True, status='No Room Available')
-
-            if person_type == 'fellow':
-                if livingspace and choice:
-                    message = template.allocated_living_message
-                    ui.print_template_room(message, name, livingspace)
-                elif choice and not livingspace:
-                    message = template.not_allocated_living_message
-                    ui.print_template_room(message, name, fail=True, status='No Room Available')
+            ui.person_ui(status_messages)
 
     def do_print_room(self, args):
         """
@@ -178,33 +130,15 @@ class Dojo(cmd.Cmd):
             file_name = docopt(self.do_print_unallocated.__doc__, args)
 
         except DocoptExit as e:
-            ui.print_message(e)
+            ui.print_error(e)
         else:
             unallocated_person = logic.list_unallocated(self.dojo)
             if file_name['<-o=FILE>']:
-                logic.save_data_txt(file_name['<-o=FILE>'], unallocated_person)
-                ui.print_message("Data saved succefully to file: " + file_name['<-o=FILE>'])
+                file_name = file_name['<-o=FILE>']
+                logic.save_data_txt(file_name, unallocated_person)
+                ui.print_success("Data saved succefully to file: " + file_name)
             else:
-                ui.print_message("*" * 40)
-                ui.print_message('      LIST OF UNALLOCATED PEOPLE')
-                ui.print_message("*" * 40)
-
-                # build display message
-                if len(unallocated_person) > 0:
-                    user_info = ''
-                    for person in unallocated_person:
-                        user_info = person.name.upper()
-                        if isinstance(person, model.Fellow):
-                            wants_living = 'N'
-                            user_info += "  FELLOW "
-                            if person.wants_living:
-                                wants_living = 'Y'
-                            user_info += wants_living
-                        else:
-                            user_info += "  STAFFF   "
-                        ui.print_not_allocated(user_info)
-                else:
-                    ui.print_message("Every one is allocated ;-)")
+                ui.unallocated_ui(unallocated_person)
 
     def do_reallocate_person(self, args):
         """
@@ -214,7 +148,7 @@ class Dojo(cmd.Cmd):
         try:
             reallocate_information = docopt(self.do_reallocate_person.__doc__, args)
         except DocoptExit as e:
-            ui.print_message(e)
+            ui.print_error(e)
         else:
             room_name = reallocate_information['<new_room_name>']
             person_id = reallocate_information['<person_id>']
@@ -230,18 +164,18 @@ class Dojo(cmd.Cmd):
             file_name = docopt(self.do_load_people.__doc__, args)
 
         except DocoptExit as e:
-            ui.print_message(e)
+            ui.print_error(e)
         else:
             file_name = file_name['<file_name>']
             status_messages = logic.load_data_txt(file_name, self.dojo)
 
             for status in status_messages:
                 if status['status'] == 'filenotfound':
-                    ui.print_message(status['message'])
+                    ui.print_error(status['message'])
                 elif status['status'] == 'illegalformat':
-                    ui.print_message(status['message'])
+                    ui.print_warning(status['message'], status='Invalid format')
                 else:
-                    self.person_ui(status)
+                    ui.person_ui(status)
 
 
 if __name__ == '__main__':
